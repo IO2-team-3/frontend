31070718b281f09dc2578f1c1851cb6feeac484c
Bartosz Dybowski <01161292@pw.edu.pl>
2023-04-23 04:39:05 +0200

    Add updating event

--- event-finder-web-client/src/constants/index.js
+++ event-finder-web-client/src/constants/index.js
@@ -212,3 +212,6 @@ export const geoCoordinatesValues = [
     max: 180 
   }
 ]
+
+export const numberOfReservationsError = "More places booked than all. This may result in the need to decline several bookings."
+
